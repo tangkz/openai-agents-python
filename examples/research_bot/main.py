70b388c175b9e0a5b9289d93e2e37ec416a9f055
Kaizhi Tang <kaizhi.tang@anywhere.re>
2025-04-01 00:43:01 -0400

    testing azure openai

--- examples/research_bot/main.py
+++ examples/research_bot/main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from .manager import ResearchManager
-
+from examples.set_azure_openai_client import *
 
 async def main() -> None:
     query = input("What would you like to research? ")
